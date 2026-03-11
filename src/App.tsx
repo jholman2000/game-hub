dbf03657adb7635958a15667f8a925a5e591701a
Jeff Holman <jholman@orionhealthcare.onmicrosoft.com>
2026-03-11 12:21:24 -0400

    Responsive grid layout initial using bootstrap-react

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,29 @@
-import Button from "react-bootstrap/Button";
+import { Container, Row, Col } from "react-bootstrap";
 
-function App() {
-  return <Button variant="primary">Primary</Button>;
+export default function App() {
+  return (
+    <Container fluid>
+      {/* Row 1: Nav (full width on all screens) */}
+      <Row>
+        <Col style={{ background: "coral" }}>Nav</Col>
+      </Row>
+
+      {/* Row 2: Aside + Main (Aside hidden on small screens) */}
+      <Row>
+        {/* Aside (hidden below lg) */}
+        <Col
+          lg={3}
+          className="d-none d-lg-block"
+          style={{ background: "gold", paddingLeft: 20, paddingRight: 20 }}
+        >
+          Aside
+        </Col>
+
+        {/* Main */}
+        <Col lg={9} style={{ background: "dodgerblue" }}>
+          Main
+        </Col>
+      </Row>
+    </Container>
+  );
 }
-
-export default App;
