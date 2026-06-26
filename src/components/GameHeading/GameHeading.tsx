464574182ffed6d52835c248aabddfbbad9672da
Jeff Holman <jholman@orionhealthcare.onmicrosoft.com>
2026-06-26 10:26:46 -0400

    Added GameHeading

--- src/components/GameHeading/GameHeading.tsx
+++ src/components/GameHeading/GameHeading.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import { GameHeadingProps } from "./GameHeading.types";
 import styles from "./GameHeading.module.css";
 
-const GameHeading: React.FC<GameHeadingProps> = ({ className, ...rest }) => {
-  //const Component = "&lt;h1&gt;" as any; // allow dynamic component type
-  const { genre, platform } = rest.gameQuery || {};
-  const stripGames = (name: string) => name.replace(/\s*Games$/i, "").trim();
+const stripGames = (name: string) => name.replace(/\s*Games$/i, "").trim();
+
+const GameHeading: React.FC<GameHeadingProps> = ({ gameQuery }) => {
+  const { genre, platform } = gameQuery;
   const heading =
     genre || platform
       ? `${genre ? stripGames(genre.name) : ""} ${platform ? stripGames(platform.name) : ""} Games`.trim()
